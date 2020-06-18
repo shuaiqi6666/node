e7c93d57ce04f2d557c149e86b91eca7dfaa7a29
肖琦 <1731455676@qq.com>
2020-06-18 22:33:40 +0800

    更新

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
-const http = require("http");
+var http = require("http");
 
-const server = http.createServer((req, res) => {
-    res.end('123');
-    // console.log(req.headers)
-})
-
-
-// 事件循环
-const bar = () => console.log('bar')
-
-const baz = () => console.log('baz')
-
-const foo = () => {
-    console.log('foo')
-    bar()
-    baz()
-}
-
-foo()
-
-
-// 指示引擎在当前操作结束时，在下一个事件循环滴答开始之前调用此函数
-process.nextTick(() => {
-    //do something
-    console.log(1)
-})
-
-setImmediate(() => {
-    //run something
-})
-
-server.listen('3000')
+http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Hello World"); response.end();
+}).listen(8888);
